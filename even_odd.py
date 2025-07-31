4215a567d10ab3373a2a0021a8d0c80fb4e56445
omkar <omkarsp20@gmail.com>
2025-07-31 14:17:03 +0530

    Adding even-odd number solutions

---
+++ even_odd.py
@@ -0,0 +1,154 @@
+# Even, Odd, and Natural Number Programs
+
+#1.Python Program to find Odd or Even
+# num = int(input("Enter any number :- "))
+# if(num%2==0):
+#     print(f"{num} is Even")
+# else:
+#     print(f"{num} is Odd.")
+
+
+#2.Python program to Print Natural number 1 to N
+# num = int(input("Enter any number :- "))
+# for i in range(1,num+1):
+#     print(i)
+
+
+#3.Python program to Print Natural Numbers in Reverse Order
+# for i in range(10,0,-1):
+#     print(i)
+
+
+#4.Python program to Print Even Numbers from 1 to 100
+# for i in range(1,101):
+#     if(i%2 == 0):
+#         print(i)
+
+
+#5.Python program to Print Odd Numbers from 1 to 100
+# for i in range(1,101):
+#     if(i%2 != 0):
+#         print(i)
+
+
+#6.Python Program to Print First 10 Even Natural Numbers
+# for i in range(2,21,2):
+#     print(i)
+
+
+#7.Python Program to Print First 10 Natural Numbers
+# for i in range(1,11):
+#     print(i)
+
+
+#8.Python Program to Print First 10 Natural Numbers in Reverse
+# for i in range(10,0,-1):
+#     print(i)
+
+
+#9.Python Program to Print First 10 Odd Natural Numbers
+# count = 0
+# for i in range(1,100):
+#     if(i%2!=0):
+#         print(i)
+#         count += 1
+#     if(count >= 10):
+#         break
+
+
+#10.Python Program to find the Sum and Average of Natural Numbers
+# total = avg = count = 0
+# for i in range(1,11):
+#     total += i
+#     count += 1
+# avg = total / count
+# print(f"Total is :- {total} \nAverage is :- {avg}")
+
+
+#11.Python Program to Read 10 Numbers and Find their Sum and Average
+# total = avg = 0
+# i = 0
+# while i < 10:
+#     num = int(input("Enter any number :- "))
+#     total += num
+#     i += 1
+# avg = total / 10
+# print(f"Total is :- {total} \nAverage is :- {avg}")
+
+
+#12.Python Program to Find the Sum of 10 Numbers and Skip Negative Numbers
+# i = 0
+# total = 0
+# while i < 10:
+#     num = int(input("Enter any number :- "))
+#     if(num > 0):
+#         total += num
+#     i += 1
+# print(f"Sum of 10 numbers :- {total}")
+
+
+#13.Python Program to Find the Sum of 10 Numbers until user enters Positive Numbers
+# i = 0
+# total = 0
+# while i < 10:
+#     num = int(input("Enter any number :- "))
+#     if(num > 0):
+#         total += num
+#         i += 1
+#         if(i == 10):
+#             break
+# print(f"Sum of all :- {total}")
+
+
+#14.Python Program to find Sum of Natural Numbers
+# total = 0
+# for i in range(1,100):
+#     total += i
+# print(f"Total is :- {total}")
+
+
+#15.Python Program to find Sum of Even Numbers
+# total = 0
+# for i in range(1,101):
+#     if(i%2==0):
+#         total += i
+# print(f"Sum of even :- {total}")
+
+
+#16.Python Program to find the Sum and Average of Natural Numbers
+# total = avg = count = 0
+# for i in range(1,101):
+#     total += i
+#     count += 1
+# avg = total / count
+# print(f"Sum of natural number is :- {total} and sum of average is :- {avg}")
+
+
+#17.Python Program to find the Sum of Odd Numbers
+# total = 0
+# for i in range(1,101):
+#     if(i%2!=0):
+#         total += i
+# print(f"Sum of Odd :- {total}")
+
+
+#18.Python Program to find the sum of Even and Odd Numbers
+# even = odd = 0
+# for i in range(1,101):
+#     if(i%2!=0):
+#         odd += i
+#     else:
+#         even += i
+# print(f"Sum of Odd :- {odd}")
+# print(f"Sum of Even :- {even}")
+
+
+#19.Python Program to Read 10 Numbers and Find their Sum and Average
+# i = 0
+# total = 0
+# while (i < 10):
+#     num = int(input("Enter any number :- "))
+#     total += num
+#     i += 1
+# avg = total / 10
+# print(f"Sum of 10 numbers :- {total} \nAverage of 10 numbers :- {avg}")
