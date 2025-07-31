9304b7b1ac818fc13ce12b67a239a6826e3c7c94
omkar <omkarsp20@gmail.com>
2025-07-31 14:37:15 +0530

    Added examples on characters

---
+++ python_characters_examples.py
@@ -0,0 +1,71 @@
+#Python Programming Examples on Characters
+
+#1.Python program to check Alphabet or not
+# ch = input("Enter any character :- ")
+# if((ch >= 'a' and ch <= 'z') or (ch >= 'A' and ch <= 'Z')):
+#     print("Yes it's an alphabet")
+# else:
+#     print("No it's not an alphabet")
+
+
+#2.Python program to check Alphabet or Digit
+# ch = input("Enter any character :- ")
+# if((ch >= 'a' and ch <= 'z') or (ch >= 'A' and ch <= 'Z')):
+#     print("Yes it's an alphabet")
+# elif((ch >= '0' and ch<= '9')):
+#     print("yes it is a Digit.")
+# else:
+#     print("Not a digit or alphabet")
+
+
+#3.Python program to check Character is an Alphabet, Digit or Special Character
+# ch = input("Enter any character :- ")
+# if((ch >= 'a' and ch <= 'z') or (ch >= 'A' and ch <= 'Z')):
+#     print("Yes it's an alphabet")
+# elif((ch >= '0' and ch<= '9')):
+#     print("yes it is a Digit.")
+# else:
+#     print("Special symbol")
+
+
+#4.Python program to check Digit or Not
+# ch = input("Enter any character :- ")
+# if((ch >= '0' and ch<= '9')):
+#     print("yes it is a Digit.")
+# else:
+#     print("not a digit")
+
+
+#5.Python program to check Lowercase or not
+# ch = input("Enter any character :- ")
+# if(ch.islower()):
+#     print("Yes it's lowercase character")
+# else:
+#     print("No it's not a lowercase character")
+
+
+#6.Python program to check Lowercase or Uppercase
+# ch = input("Enter any character :- ")
+# if(ch.islower()):
+#     print("Yes it's lowercase character")
+# elif(ch.isupper()):
+#     print("Yes it's Uppercase character")
+# else:
+#     print("Not in uppercase or lowercase")
+
+#7.Python program to check Uppercase or not
+# ch = input("Enter any character :- ")
+# if(ch.isupper()):
+#     print("Yes it's Uppercase character")
+# else:
+#     print("No it's not a Uppercase characters")
+
+
+#8.Python program to check Vowel or Consonant
+# ch = input("Enter any character :- ")
+# vowel = ['a','e','i','o','u','A','E','I','O','U']
+# if(ch.isalpha()):
+#     if(ch in vowel):
+#         print("yes it is vowel")
+#     else:
+#         print("No its not a vowel")
