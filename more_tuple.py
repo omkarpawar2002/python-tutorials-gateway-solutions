074b89af91bc89cdc4eeebcf831fb7e354b75b93
omkar <omkarsp20@gmail.com>
2025-08-18 18:03:13 +0530

    Add Some extra Tuple Solutions

---
+++ more_tuple.py
@@ -0,0 +1,71 @@
+#Tuple Items printing programs
+
+#1.Get Tuple Items
+# t = (10,20,30,40,50)
+# print(t[0])
+# print(t[1])
+# print(t[2])
+# print(t[3])
+# print(t[4])
+
+
+#2.Python Program to Print Tuple using string formatting
+# t = (10,20,30,40,50)
+# print(f"Here is a tuple :- {t}")
+
+
+#3.Python Program to Print Even Numbers in Tuple
+# t = (1,2,3,4,5,6,7,8,9,10)
+# for i in t:
+#     if(i%2==0):
+#         print(i)
+
+
+#4.Python Program to Print Negative Numbers in Tuple
+# t = (10,20,30,40,50,-1,2,-3,45)
+# for i in t:
+#     if(i<0):
+#         print(i)
+
+
+#5.Python Program to Print Positive Numbers in Tuple
+# t = (10,20,30,40,50,-1,2,-3,45)
+# for i in t:
+#     if(i>0):
+#         print(i)
+
+
+#6.Python Program to Print Odd Numbers in Tuple
+# t = (10,20,30,40,50,-1,2,-3,45)
+# for i in t:
+#     if(i%2!=0):
+#         print(i)
+
+
+#7.Python Program to Print Smallest Item in a Tuple
+# t = (10,20,30,40,50,-1,2,-3,45)
+# print(min(t))
+
+
+#8.Python Program to Print Largest Item in a Tuple
+# t = (10,20,30,40,50,-1,2,-3,45)
+# print(max(t))
+
+
+#9.Python Program to Find Largest and Smallest Item in a Tuple
+# t = (10,20,30,40,50,-1,2,-3,45)
+# small = t[0]
+# large = t[0]
+# for i in range(len(t)):
+#     if(t[i] > large):
+#         large = t[i]
+#     elif(t[i] < small):
+#         small = t[i]
+# print("Small :- ",small)
+# print("large :- ",large)
+
+
+#10.Python Program to Print Tuple Items
+# t = (10,20,30,40,50)
+# for i in t:
+#     print(i)
