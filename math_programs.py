873d5cb022e11178a8e68f02a209708a2a9efb43
omkar <omkarsp20@gmail.com>
2025-09-25 18:39:49 +0530

    Added some math related problems

---
+++ math_programs.py
@@ -0,0 +1,122 @@
+#1.Python program to find the Cube of a Number
+# num = int(input("Enter any number :- "))
+# print(f"Cube of {num} is :- {num**3}")
+
+
+#2.Python program to find Profit Or Loss
+# cost_price = float(input("Enter cost price :- "))
+# selling_price = float(input("Enter selling price :- "))
+# if(cost_price < selling_price):
+#     profit = selling_price - cost_price
+#     print(f"Profit is :- {profit}")
+# elif(cost_price > selling_price):
+#     loss = cost_price - selling_price
+#     print(f"Loss is :- {loss}")
+
+
+#3.Python program to find the Square of a Number
+# num = int(input("Enter any number :- "))
+# print(f"Square of {num} is {num**2}")
+
+
+#4.Python program to find the Square root of a Number
+# import math
+# num = int(input("Enter any number :- "))
+# print(f"Square root of {num} is {math.sqrt(num)}")
+
+
+#5.Python Program to find all divisors of an integer
+# num = int(input("Enter any number :- "))
+# print(f"Here is all divisors of {num}")
+# for i in range(1,num+1):
+#     if(num%i==0):
+#         print(i)
+
+
+#6.Python Program to Find Compound Interest
+
+
+#7.Python Program to Calculate Electricity Bill
+
+
+#8.Python Program to Find Distance Between Two Points
+
+
+#9.Python program to find the Power of a Number
+# num = int(input("Enter any number :- "))
+# power = int(input("Enter power to number :- "))
+# print(f"{num} to the power of {power} is :- {num**power}")
+
+
+#10.Python program for Multiplication Table
+# num = int(input("Enter any number :- "))
+# for i in range(1,11):
+#     print(f"{num} * {i} = {num*i}")
+
+
+#11.Python Program to find Roots of a Quadratic Equation
+
+
+#12.Python Program to find Student Grade
+# s1 = float(input("Enter first subject marks :- "))
+# s2 = float(input("Enter second subject marks :- "))
+# s3 = float(input("Enter third subject marks :- "))
+# s4 = float(input("Enter fourth subject marks :- "))
+# s5 = float(input("Enter fifth subject marks :- "))
+# total = s1 + s2 + s3 + s4 + s5
+# percentage = round((total / 500) * 100)
+# print(percentage,2)
+# grade = ''
+# if(percentage >= 95):
+#     grade = 'O'
+# elif(percentage >= 90):
+#     grade = 'A+'
+# elif(percentage >= 80):
+#     grade = 'A'
+# elif(percentage >= 70):
+#     grade = 'B+'
+# elif(percentage >= 60):
+#     grade = 'B'
+# elif(percentage >= 35):
+#     grade = 'C'
+# else:
+#     grade = 'F'
+# print(f"Student Got percentage is {percentage} with {grade} grade.")
+
+
+#13.Python Program to find Simple Interest
+# p = float(input("Enter principle amount :- "))
+# r = float(input("Enter interest rate :- "))
+# t = int(input("Enter time period :- "))
+# si = (p * r * t) / 100
+# print(f"Simple Interest is :- {si}")
+
+
+#14.Python program to find the Total Average & Percentage of 5 Subjects
+# s1 = float(input("Enter first subject marks :- "))
+# s2 = float(input("Enter second subject marks :- "))
+# s3 = float(input("Enter third subject marks :- "))
+# s4 = float(input("Enter fourth subject marks :- "))
+# s5 = float(input("Enter fifth subject marks :- "))
+# total = s1 + s2 + s3 + s4 + s5
+# avg = total / 5
+# percentage = round((total / 500) * 100)
+# print(f"Total average is :- {avg} and total percentage is :- {percentage}")
+
+
+#15.Python Program to Make a Simple Calculator
+# num1 = int(input("Enter first number :- "))
+# num2 = int(input("Enter second number :- "))
+# op = input("Enter any operator :- ")
+# res = 0
+# if(op == '+'):
+#     res = num1 + num2
+# elif(op == '-'):
+#     res = num1 - num2
+# elif(op == '*'):
+#     res = num1 * num2
+# elif(op == '/'):
+#     res = num1 / num2
+# elif(op == '**'):
+#     res = num1 **  num2
+# print(f"Here is the result of calculator :- {res}")
